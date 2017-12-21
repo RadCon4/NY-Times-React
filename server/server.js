8881cc876921a42b87e5365fe2c603d58a729e21
Conrad Harrison <a.conradharrison@gmail.com>
2017-12-21 13:30:40 -0600

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ app.use(express.static("client/build"));
 
 // -------------------------------------------------
 
-
-mongoose.connect("mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk");
+mongoose.connect("mongodb://radcon4:leahkr_12@ds023593.mlab.com:21166/nytreact");
+// mongoose.connect("mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk");
 
 var db = mongoose.connection;
 
